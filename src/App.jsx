87fc6eb4f5b57c07fcc981a361fe2fe444f476fd
mlim20 <mlim20@student.ubc.ca>
2026-05-18 23:25:31 -0700

    minor edits to App.jsx, added skeleton Footer files

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
-import React from 'react'
-import Navbar from './components/Navbar'
+import React, { useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 import Home from './pages/Home'
 import About from './pages/About'
 import Materials from './pages/Materials'
 import Gallery from './pages/Gallery'
+import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -25,6 +22,7 @@ function App() {
           <Route path="/materials" element={<Materials />}></Route>
           <Route path="/gallery" element={<Gallery />}></Route>
         </Routes>
+        <Footer />
       </BrowserRouter>
     </>
   )
---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+    return (
+        <div>
+            <p><a href="https://github.com/mlim20/crochet-things">Link to repository</a></p>
+        </div>
+    )
+}
+
+export default Footer
