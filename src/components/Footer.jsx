d68232e7d0989d75da5bea84cd604c0c606826f9
mlim20 <mikaylalim20@gmail.com>
2026-06-01 20:19:11 -0700

    grid on materials page responsive, navbar responsive, footer opens new tab

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import './Footer.css'
 const Footer = () => {
     return (
         <div className="footer-container">
-            <a href="https://github.com/mlim20/crochet-things">Link to repository</a>
+            <a href="https://github.com/mlim20/crochet-things" target="_blank">Link to repository</a>
         </div>
     )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,36 @@
-import React from 'react'
+import React, { useState} from 'react'
 import { BrowserRouter, NavLink } from 'react-router-dom'
+import { IoMenuSharp as MenuIcon } from "react-icons/io5";
+import { IoMdClose as CloseMenu } from "react-icons/io";
 import './Navbar.css'
 import About from '../pages/About'
 
 const Navbar = () => {
+    // Responsive navbar:
+    // Adapted from https://sdever.medium.com/building-a-responsive-navigation-bar-with-react-css-d9b30af03c20
+    // and https://medium.com/@badalchandrahalder99/how-to-create-a-responsive-navbar-in-react-2bbc51081571
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const handleIsMenuOpen = () => setIsMenuOpen(!isMenuOpen);
+    const closeMenu = () => setIsMenuOpen(false);
+
     return (
-        <nav className="navbar">
-            <ul className="navlist">
-                <li><NavLink to="/">Home</NavLink></li>
-                <li><NavLink to="about">About</NavLink></li>
-                <li><NavLink to="materials">Materials</NavLink></li>
-                <li><NavLink to="gallery">Gallery</NavLink></li>
+        <nav className={isMenuOpen ? "navbar menu-active" : "navbar"}>
+            <div className="menu" onClick={handleIsMenuOpen}>
+                {
+                    isMenuOpen ? (
+                        <CloseMenu className="menu-icon close" />
+                    ) : (
+                        <MenuIcon className="menu-icon icon"/>
+                    )
+                }
+            </div>
+            <ul className={isMenuOpen ? "navlist menu-active" : "navlist"}>
+                <li><NavLink to="/" onClick={closeMenu}>Home</NavLink></li>
+                <li><NavLink to="about" onClick={closeMenu}>About</NavLink></li>
+                <li><NavLink to="materials" onClick={closeMenu}>Materials</NavLink></li>
+                <li><NavLink to="gallery" onClick={closeMenu}>Gallery</NavLink></li>
             </ul>
         </nav>
-        // <nav className="navbar">
-        //     <ul className="navlist">
-        //         <li><a href="/">Home</a></li>
-        //         <li><a href="/about">About</a></li>
-        //         <li><a href="/materials">Materials</a></li>
-        //         <li><a href="/gallery">Gallery</a></li>
-        //     </ul>
-        // </nav>
     )
 }
 
